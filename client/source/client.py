d70c98364b3d79080d3efe9d163d4515f915feee
Rachit Garg <rgarg@HPLaptop.(none)>
2015-02-23 22:19:13 -0500

    Updated decorations of Client program

--- client/source/client.py
+++ client/source/client.py
@@ -1,5 +1,48 @@
+usage_text = """
+LPIRC Client 
+====================
+@2015 - HELPS, Purdue University
+
+Main Tasks:
+-----------
+1. Login to the Server and store the token generated.
+2. Requests for images from server and stores them in directory.
+3. POSTS multiple result lines to the server in the form of result.
+
+Requirements:
+-------------
+1. Python v2.7.3
+2. PycURL
+
+Usage:
+------
+client.py [OPTION]...
+Options:
+         -w, --ip
+                IP address of the server in format <xxx.xxx.xxx.xxx>
+                Default: 127.0.0.1
+
+         -p, --port
+                Port number of the server.
+                Default: 5000
+
+         --user
+                Username
+                Default: lpirc
+
+         --pass
+                Password for the username
+                Default: pass
+
+         -h, --help
+                Displays all the available option
+
+
+"""
+
+
 import pycurl
-import pycurl
+import getopt,sys
 try:
     # python 3
     from urllib.parse import urlencode
@@ -8,15 +51,77 @@ except ImportError:
     from urllib import urlencode
 
 from StringIO import StringIO as BytesIO
-
-
 from StringIO import StringIO
+
+def get_image(token, number):
+	c = pycurl.Curl()
+	c.setopt(c.URL, host_ipaddress+':'+host_port+'/image/?image='+str(number))
+	post_data = {'token':token}
+	postfields = urlencode(post_data)
+	c.setopt(c.POSTFIELDS,postfields)
+	with open('image'+str(number)+'.jpg', 'w') as f:
+    		c.setopt(c.WRITEDATA, f)
+    		c.perform()
+    		c.close()
+
+def post_result(token, data):
+	c = pycurl.Curl()
+	c.setopt(c.URL, host_ipaddress+':'+host_port+'/result')
+	post_data = {'token':token}
+	postfields = urlencode(post_data)+'&'+urlencode(data,True)
+	print postfields
+	c.setopt(c.POSTFIELDS,postfields)
+   	c.perform()
+    	c.close()
+
+# Script usage function
+def usage():
+    print usage_text
+
+# Main function to parse command-line input and run server
+def parse_cmd_line():
+
+    global host_ipaddress
+    global host_port
+    global username
+    global password
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hw:p:", ["help", "ip=", "port=", "user=", "pass="])
+    except getopt.GetoptError as err:
+        print str(err) 
+        usage()
+        sys.exit(2)
+    for switch, val in opts:
+        if switch in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif switch in ("-w", "--ip"):
+            host_ipaddress = val
+        elif switch in ("-p", "--port"):
+            host_port = val
+        elif switch == "--user":
+            username = val
+        elif switch == "--pass":
+            password = val
+        else:
+            assert False, "unhandled option"
+
+    print "\nhost = "+host_ipaddress+":"+host_port+"\nUsername = "+username+"\nPassword = "+password+"" 
+
+
+# Parse Command-line
+host_ipaddress = '127.0.0.1'
+host_port = '5000'
+password = 'pass'
+username = 'lpirc'
+parse_cmd_line()
 
 #login
 buffer = StringIO()
 c = pycurl.Curl()
-c.setopt(c.URL, 'http://127.0.01:5000/login')
-post_data = {'username':'lpirc','password':'pass'}
+c.setopt(c.URL, host_ipaddress+':'+host_port+'/login')
+post_data = {'username':username,'password':password}
 postfields = urlencode(post_data)
 c.setopt(c.POSTFIELDS,postfields)
 c.setopt(c.WRITEFUNCTION, buffer.write)
@@ -29,33 +134,10 @@ token = buffer.getvalue()
 print(token)
 
 
-def get_image(token, number):
-	c = pycurl.Curl()
-	c.setopt(c.URL, 'http://127.0.01:5000/image/?image='+str(number))
-	post_data = {'token':token}
-	postfields = urlencode(post_data)
-	c.setopt(c.POSTFIELDS,postfields)
-	with open('image'+str(number)+'.jpg', 'w') as f:
-    		c.setopt(c.WRITEDATA, f)
-    		c.perform()
-    		c.close()
-
-def post_data(token, data):
-	c = pycurl.Curl()
-	c.setopt(c.URL, 'http://127.0.01:5000/result')
-	post_data = {'token':token}
-	postfields = urlencode(post_data)+'&'+urlencode(data,True)
-	print postfields
-	c.setopt(c.POSTFIELDS,postfields)
-   	c.perform()
-    	c.close()
-
-
-
 
 get_image(token,1)
 data = {'image_name':'picasa','CLASS_ID':'12','confidence':'0.3','xmin':'100','ymin':'10','xmax':'500','ymax':'200'}
-post_data(token,data)
+post_result(token,data)
 
 	
 		
